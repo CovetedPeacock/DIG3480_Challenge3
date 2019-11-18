4282526b1f711119788499288a11f1b0dd665931
CovetedPeacock <coveted.peacock@gmail.com>
2019-11-17 20:55:38 -0500

    Initial Commit

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+
+    private void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards [Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+            {
+                restartText.text = "Press 'E' to Restart!";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            gameOverText.text = "You win! Game created by Connor Peacock.";
+            gameOver = true;
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over! Game created by Connor Peacock.";
+        gameOver = true;
+    }
+}
